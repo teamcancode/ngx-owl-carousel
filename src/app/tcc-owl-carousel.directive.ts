0b354815524b3a0fb0196d7d19de274f07f617f4
Binh Nguyen <huubinh.nguyen@officience.com>
2017-07-19 08:15:33 +0700

    update _ngcontent attribute

--- src/app/tcc-owl-carousel.directive.ts
+++ src/app/tcc-owl-carousel.directive.ts
@@ -9,9 +9,11 @@ export class TccOwlCarouselDirective implements AfterViewInit {
     private _element: any;
     private _dataOwl: any;
     private _config: Object;
+    private _ngContentAttribute: string;
 
     constructor(private _elementRef: ElementRef) {
         if (this.isClient()) {
+            this._ngContentAttribute = '';
             this._dataOwl = null;
             this._element = $(this._elementRef.nativeElement);
         }
@@ -36,18 +38,42 @@ export class TccOwlCarouselDirective implements AfterViewInit {
     }
 
     private initSlide(configs: Object): void {
+        if (0 === this._element.length) {
+            return;
+        }
+
+        const attributes: any = this._element[0].attributes;
+        for (const attribute of attributes) {
+            const attributeName: string = attribute.name;
+            if (0 === attributeName.indexOf('_ngcontent-')) {
+                this._ngContentAttribute = attributeName;
+                break;
+            }
+        }
+
         this._element.addClass('owl-carousel');
         this._element.owlCarousel(configs || {});
         this._dataOwl = this._element.data('owl.carousel');
+        this.updateNgContent();
     }
 
     private refreshSlide(): void {
+        if (0 === this._element.length) {
+            return;
+        }
         this._element.trigger('refresh.owl.carousel');
+        this.updateNgContent();
     }
 
     private updateConfig(key: string, value: any): void {
         this._dataOwl.settings[key] = value;
         this._dataOwl.options[key] = value;
+    }
+
+    private updateNgContent() {
+        this._element.find('*').each((index, element) => {
+            $(element).attr(this._ngContentAttribute, '');
+        });
     }
 
     //noinspection JSMethodCanBeStatic
